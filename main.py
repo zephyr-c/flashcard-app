6189c583b03433f534c1b5272960ec0d234e598e
Zephyr <z.worthington@outlook.com>
2021-02-14 13:40:49 -0800

    Create pandas dataframe for french to english words from CSV file. Write function to randomly choose a new word and display it in the word text on the flashcard image.

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from tkinter import *
+import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Ariel"
+NEW_LANGUAGE = "French"
+
+# ---------------------------- NEW CARD ------------------------------- #
+french_dataframe = pd.read_csv("data/french_words.csv")
+french_words = french_dataframe.to_dict(orient="records")
+def new_card():
+    flashcard = random.choice(french_words)
+    canvas.itemconfigure(word, text=flashcard['French'])
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -12,18 +22,18 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 flashcard_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=flashcard_front, anchor="center")
-canvas.create_text(400, 150, text="Title", font=(FONT_NAME, 40, "italic"))
-canvas.create_text(400, 263, text="Word", font=(FONT_NAME, 60, "bold"))
+title = canvas.create_text(400, 150, text=NEW_LANGUAGE, font=(FONT_NAME, 40, "italic"))
+word = canvas.create_text(400, 263, text="Word", font=(FONT_NAME, 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 
 # Buttons
 wrong_img = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=new_card)
 wrong_button.grid(row=1, column=0)
 
 right_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_img, highlightthickness=0)
+right_button = Button(image=right_img, highlightthickness=0, command=new_card)
 right_button.grid(row=1, column=1)
 
 # Labels
